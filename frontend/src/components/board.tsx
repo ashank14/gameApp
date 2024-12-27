680843873cd77ec713d068e99c631f6adb0d3277
Ashank sethi <ashank@Ashanks-MacBook-Air.local>
2024-12-28 01:32:22 +0530

    board

--- frontend/src/components/board.tsx
+++ frontend/src/components/board.tsx
@@ -1,9 +1,75 @@
 
+import { useEffect, useState } from "react";
 
-function Board(){
+function Board({ socket,loading, turn }: { socket: WebSocket|null, loading:boolean, turn:string }){
+
+
+    const [move,setMove]=useState<"X"|"O">("X");
+    const [state,setState]=useState(["","O","","","","","","",""]); 
+
+    useEffect(()=>{
+        //listen for game state updates
+
+        if(!socket){
+            return;
+        }
+
+        socket.addEventListener("message",(event)=>{
+            const data=JSON.parse(event.data);
+            if(data.res="game-state"){
+                console.log("received state:",data);
+                setMove(data.currentTurn);
+                setState(data.message);
+            }
+        })
+
+
+    },[]);
+
+    function handleClick(i:number){
+        if(!socket){
+            return;
+        }
+        if(move!=turn||state[i]!=""){
+            return;
+        }
+        //change move after placing
+        //the listener will update the move after p2 makes a move
+
+        setState(state.map((item, index) => index === i ? turn : item));
+        
+        move==="X"?setMove("O"):setMove("X");
+
+        socket.send(JSON.stringify({
+            event:"game-state",
+            message:state,
+            currentTurn:move,
+            roomid:localStorage.getItem("roomid")
+
+        }))
+    }
+
+    function check(){
+
+    }
+
     return(
         <>
-            Board
+            {loading?<div>Loading board</div>:            
+            <div className="w-full h-full flex justify-between items-center">
+                <div>P1</div>
+
+                <div className="border w-[50%] h-[80%] grid grid-cols-3">
+                    {
+                        state.map((move,index)=>(
+                            <button onClick={()=>handleClick(index)}>{move}</button>
+                        ))                 
+                    }
+                </div>
+
+                <div>P2</div>
+            </div>}
+
         </>
     )
 }
--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 function Game(){
 
     const [socket, setSocket] = useState<WebSocket | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);  
+    const [loading, setLoading] = useState<boolean>(false);  
     const roomevent = useParams();
 
     useEffect(() => {
@@ -14,7 +14,6 @@ function Game(){
 
         const socket = new WebSocket("ws://localhost:3000");
         setSocket(socket);
-        console.log(socket);
         socket.addEventListener("open", (event) => {
             socket.send(
                 JSON.stringify({
@@ -35,12 +34,11 @@ function Game(){
         };
     }, []); 
 
-
     return(    
         <>
             <div className="bg-black w-full h-screen text-white grid lg:grid-cols-[2fr,1.5fr] grid-rows-none">
                 <div>
-                    <Board/>
+                    <Board socket ={socket} loading={loading} turn = {roomevent.event==="create-room"?"X":"O"}/>
                 </div>
                 <div>
                     <Chat socket={socket} loading={loading}/>
